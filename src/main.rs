e29f3b6383f5de757c79f31a74a79672e4f98b4f
Christian Steimel <me@C14L.com>
2020-05-17 19:25:57 +0200

    Rotate

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 use rand::seq::SliceRandom;
 use std::io::{stdout, Write};
+use std::process::exit;
 use std::time;
 
 use crossterm::{
@@ -25,28 +26,28 @@ const YMARGIN: usize = 2;
 const PIXEL_EMPTY: u8 = b' ';
 const PIXEL_SOLID: u8 = b'X';
 const PIXEL_BORDER: u8 = b'#';
-const BRICKS: [&[u8]; 7] = [
-    b" X   X   X   X  ",
-    b" XX  XX         ",
-    b" X   X   XX     ",
-    b"  X   X  XX     ",
-    b"XX   XX         ",
-    b"  XX XX         ",
-    b" X   XX  X      ",
+const BRICKMAX: usize = 4;
+const BRICKS: [&[u8; BRICKMAX*BRICKMAX]; 7] = [
+    b" X   X   X   X  ", // |
+    b" XX  XX         ", // o
+    b" X   X   XX     ", // |_
+    b"  X   X  XX     ", // _|
+    b"XX   XX         ", // Z
+    b"  XX XX         ", // S
+    b" X   XX  X      ", // T
 ];
 
 fn main() -> Result<()> {
-    let interval = time::Duration::from_millis(200);
-    // let now = time::Instant::now();
-
     let _ = terminal::enable_raw_mode()?;
+
     let mut is_bucket_full: bool = false;
     let mut is_brick_falling: bool;
     let mut is_free_fall: bool;
     let mut is_paused = false;
-
     let mut field: [u8; FIELD_SIZE] = [PIXEL_EMPTY; FIELD_SIZE];
     let mut score: usize = 0;
+    let mut speed: u64 = 400;
+    let mut interval: time::Duration;
     let mut stdout = stdout();
 
     stdout.execute(terminal::Clear(terminal::ClearType::All))?;
@@ -77,23 +78,26 @@ fn main() -> Result<()> {
     // Game loop
 
     while !is_bucket_full {
-        let mut brick = BRICKS.choose(&mut rand::thread_rng()).expect("Raarrrr!");
+        let mut brick: [u8; 16] = *BRICKS
+            .choose(&mut rand::thread_rng())
+            .expect("Raarrrr!")
+            .clone();
         let mut y_brick: usize = 0;
-        let mut x_brick: usize = XMAX / 2 - 2;
+        let mut x_brick: usize = XMAX / 2 - BRICKMAX / 2;
+
+        interval = time::Duration::from_millis(speed);
         is_brick_falling = true;
         is_free_fall = false;
 
         // Per-brick loop
 
         while is_brick_falling {
-            // thread::sleep(interval);
-
             // Clean up block before movement
 
-            for yb in 0..4 {
-                for xb in 0..4 {
-                    if brick[xb + yb * 4] == PIXEL_SOLID {
-                        field[(x_brick + xb) + (y_brick + yb) * XMAX] = b" "[0];
+            for yb in 0..BRICKMAX {
+                for xb in 0..BRICKMAX {
+                    if brick[xb + yb * BRICKMAX] == PIXEL_SOLID {
+                        field[(x_brick + xb) + (y_brick + yb) * XMAX] = PIXEL_EMPTY;
                     }
                 }
             }
@@ -102,17 +106,27 @@ fn main() -> Result<()> {
 
             if !is_free_fall && poll(interval)? {
                 let event = read()?;
-                if event == Event::Key(KeyCode::Char('q').into()) {
+                if event == Event::Key(KeyCode::Esc.into()) {
                     let _ = terminal::disable_raw_mode()?;
-                    panic!("Quit!");
+                    exit(0x0);
                 }
                 if event == Event::Key(KeyCode::Char('p').into()) {
                     is_paused = !is_paused;
                 }
-                if event == Event::Key(KeyCode::Left.into()) && x_brick > 1 {
-                    // TODO: here we need collison detecktion, because the brick's
-                    // x=0 axis may be empty an should not hit the left wall!
-                    x_brick -= 1;
+                if event == Event::Key(KeyCode::Left.into()) {
+                    for xb in 0..BRICKMAX {
+                        for yb in 0..BRICKMAX {
+                            let field_idx = (x_brick + xb) + (y_brick + yb) * XMAX;
+
+                            if field_idx < FIELD_SIZE {
+                                if brick[xb + yb * BRICKMAX] != PIXEL_EMPTY {
+                                    if field[field_idx] != PIXEL_EMPTY {
+                                        x_brick -= 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
                 if event == Event::Key(KeyCode::Right.into()) && x_brick < XMAX - 3 {
                     // TODO: here we need collison detecktion, because the brick's
@@ -121,6 +135,14 @@ fn main() -> Result<()> {
                 }
                 if event == Event::Key(KeyCode::Up.into()) {
                     // rotate 90deg cw
+                    let mut prev_brick = [PIXEL_EMPTY; 16];
+                    for yb in 0..BRICKMAX {
+                        for xb in 0..BRICKMAX {
+                            prev_brick[xb + yb * BRICKMAX] =
+                                brick[12 + yb - (xb * BRICKMAX)].clone();
+                        }
+                    }
+                    brick = prev_brick.clone();
                 }
                 if event == Event::Key(KeyCode::Down.into()) {
                     is_free_fall = true;
